0873340fc680e8fef73febb3da84fd8febd9a76f
liwei <tigerkinlv@outlook.com>
2023-08-23 18:02:48 +0800

    新增gui调试

--- src/views/Demo1.vue
+++ src/views/Demo1.vue
@@ -3,6 +3,7 @@
 <script setup>
 import * as THREE from "three"
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'; // 导入控制器
+import {GUI} from "three/addons/libs/lil-gui.module.min.js"; // 参数管理工具
 /*
 * 步骤
 * 1.创建场景
@@ -24,6 +25,7 @@ const parentCube=new THREE.Mesh(parentGeometry,parentMaterial);
 /* 设置物体坐标 */
 parentCube.position.set(-2,0,0); // 坐标平移
 // parentCube.scale.set(1,2,1)
+parentCube.rotation.x=Math.PI/4
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 /* 使用材质 */
@@ -55,13 +57,56 @@ controls.dampingFactor=0.03
 function animate() {
   requestAnimationFrame( animate );
   controls.update();
-
   // cube.rotation.x += 0.01;
   // cube.rotation.y += 0.01;
   renderer.render( scene, camera );
-
 }
+
 animate();
+
+/* 监听窗口变化 */
+window.addEventListener("resize",()=>{
+  // 重置相机宽高比
+renderer.setSize(window.innerWidth,window.innerHeight);
+// 重置相机宽高比
+camera.aspect=window.innerWidth/window.innerHeight;
+// 更新相机投影矩阵
+camera.updateProjectionMatrix();
+})
+
+// 全屏和退出全屏
+
+const fullScreenEvent={
+  FullScreen:function (){
+    document.body.requestFullscreen();
+  },
+  ExitFullScreen:function (){
+    document.exitFullscreen();
+  }
+}
+
+const gui=new GUI();
+
+gui.add(fullScreenEvent,"FullScreen").name("全屏");
+gui.add(fullScreenEvent,"ExitFullScreen").name("退出全屏");
+
+let folder=gui.addFolder("立方体位置")
+folder.add(cube.position,"x").min(-10).max(10).step(1).name("子立方体x轴位置");
+folder.add(cube.position,"y").min(-10).max(10).step(1).name("子立方体y轴位置");
+folder.add(cube.position,"z").min(-10).max(10).step(1).name("子立方体z轴位置");
+
+gui.add(folder)
+
+gui.add(parentMaterial,"wireframe").name("父立方体线框模式")
+
+let colorParams={
+  cubeColor:"#00ff00"
+}
+
+gui.addColor(colorParams,'cubeColor').name("立方体颜色").onChange((val)=>{
+  cube.material.color.set(val)
+})
+
 </script>
 
 <style lang="scss" src="../assets/common.scss"></style>
